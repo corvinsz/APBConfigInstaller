07025bb973a9d83f59b11874d42354e5a08f3522
Corvin <corvinszimion@googlemail.com>
2025-03-26 18:26:06 +0100

    WIP: added vpk upload to upload releases

--- velopackBuildScript.bat
+++ velopackBuildScript.bat
@@ -10,9 +10,17 @@ if "%~1"=="" (
 set "version=%~1"
 
 echo.
-echo Compiling VelopackCSharpWpf with dotnet...
+echo Installing vpk CLI if needed
+dotnet tool update -g vpk
+
+echo.
+echo Compiling app with dotnet...
 dotnet publish .\APBConfigInstaller\APBConfigInstaller\APBConfigInstaller.csproj -c Release -r win-x64 -o .\APBConfigInstaller\APBConfigInstaller\publish
 
 echo.
 echo Building Velopack Release v%version%
 vpk pack -u APBConfigInstaller -v %version% -p .\APBConfigInstaller\APBConfigInstaller\publish -e APBConfigInstaller.exe
+
+echo.
+echo Upload the generated files to a new GitHub release
+vpk upload github --repoUrl "https://github.com/corvinsz/APBConfigInstaller" --token "$env:VPK_TOKEN" --publish --releaseName "MyApp $VERSION" --tag "v$VERSION"
