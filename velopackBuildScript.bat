5a978d64b3a851b06dca5ab10c29d7007433fbfb
Corvin <corvinszimion@googlemail.com>
2025-03-26 17:31:19 +0100

    changed build script to accept a version number

--- velopackBuildScript.bat
+++ velopackBuildScript.bat
@@ -1,2 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%~1"=="" (
+    echo Version number is required.
+    echo Usage: build.bat [version]
+    exit /b 1
+)
+
+set "version=%~1"
+
+echo.
+echo Compiling VelopackCSharpWpf with dotnet...
 dotnet publish .\APBConfigInstaller\APBConfigInstaller\APBConfigInstaller.csproj -c Release -r win-x64 -o .\APBConfigInstaller\APBConfigInstaller\publish
-vpk pack -u APBConfigInstaller -v 0.1.8 -p .\APBConfigInstaller\APBConfigInstaller\publish -e APBConfigInstaller.exe
+
+echo.
+echo Building Velopack Release v%version%
+vpk pack -u APBConfigInstaller -v %version% -p .\APBConfigInstaller\APBConfigInstaller\publish -e APBConfigInstaller.exe
