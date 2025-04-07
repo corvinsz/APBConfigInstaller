9b99acf7af2fa6ba5f83d00885f251aaf079cac5
Corvin <corvinszimion@googlemail.com>
2025-04-07 22:00:28 +0200

    -updating with velopack via github releases now works -upgraded velopack to latest prerelease -added build script

--- APBConfigInstaller/Program.cs
+++ APBConfigInstaller/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Avalonia;
+using Velopack;
 
 namespace APBConfigInstaller;
 
@@ -9,8 +10,26 @@ internal sealed class Program
 	// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
 	// yet and stuff might break.
 	[STAThread]
-	public static void Main(string[] args) => BuildAvaloniaApp()
-		.StartWithClassicDesktopLifetime(args);
+	public static void Main(string[] args)
+	{
+		try
+		{
+			// It's important to Run() the VelopackApp as early as possible in app startup.
+			VelopackApp.Build()
+				.OnFirstRun((v) => { /* Your first run code here */ })
+				// TODO impl logger
+				//.SetLogger(Log)
+				.Run();
+
+			BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+		}
+		catch (Exception ex)
+		{
+
+			throw;
+		}
+
+	}
 
 	// Avalonia configuration, don't remove; also used by visual designer.
 	public static AppBuilder BuildAvaloniaApp()
